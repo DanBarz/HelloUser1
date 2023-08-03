ee6973d6dc4bfe4efdd69f2406546e9c246dd6ba
Daniel Allen <danielpeterallen@gmail.com>
2023-08-04 09:26:36 +1000

    Fiished the header

--- inputs.h
+++ inputs.h
@@ -1,4 +1,7 @@
-//this is a promise 
+//the # means we are speaking with the preprocesser 
+#ifndef PROGRAM_H // this is a header guard
+#define PROGRAM_H
+
 #include <string>
 using std::string;
 
@@ -7,3 +10,5 @@ int read_int(string prompt);
 string collect_string (string prompt);
 void output_person(string name, int age);
 double output_air_speed(int frequency, int amp);
+
+#endif
